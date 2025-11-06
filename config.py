4bc3494126c347cca4e0a424a6e2fc724bc01238
HormonyTech <aleksanderdpi@gmail.com>
2025-11-07 00:39:41 +0400

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Токен бота
-BOT_TOKEN = 'YOU_BOT_TOKEN'  # test token
+BOT_TOKEN = '8203614183:AAHeOpEq_M1wPA_IWFI4XzKygEA_MOOg6mA'  # test token
 
 # Список администраторов
-ADMIN_USERS = [Frist_admin, Second_admin]
+ADMIN_USERS = [Frist_admin, Second_admin, DPIAI]
 
 # ID для отправки уведомлений администраторам
 ADMIN_MESSAGE = Message_admin
