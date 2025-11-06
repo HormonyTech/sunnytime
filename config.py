b7d340a3b4c1232fe4787da111dcbbdbd734cd84
HormonyTech <aleksanderdpi@gmail.com>
2025-11-07 00:44:49 +0400

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BOT_TOKEN = '8203614183:AAHeOpEq_M1wPA_IWFI4XzKygEA_MOOg6mA'  # test token
 
 # Список администраторов
-ADMIN_USERS = [Frist_admin, Second_admin, DPIAI]
+ADMIN_USERS = [DPIAI]
 
 # ID для отправки уведомлений администраторам
-ADMIN_MESSAGE = Message_admin
+ADMIN_MESSAGE = 8355862725
